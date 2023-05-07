7ad4f260b9d94fd59a8a2c4999fdcfc70b3dc650
rasor <soren@rasor.dk>
2023-05-07 12:16:40 +0200

    Linkedin link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby";
 import logo from "../img/logo-txt.svg";
 import facebook from "../img/social/facebook.svg";
 import instagram from "../img/social/instagram.svg";
+import linkedin from "../img/social/l-in.svg";
 
 const Footer = () => {
   
@@ -72,17 +73,24 @@ const Footer = () => {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://www.facebook.com/ultralandsholdet" target="_blank" rel="noreferrer">
+                <a title="Facebook" href="https://www.facebook.com/ultralandsholdet" target="_blank" rel="noreferrer">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
-                <a title="instagram" href="https://www.instagram.com/ultralandsholdet_/" target="_blank" rel="noreferrer">
+                <a title="Instagram" href="https://www.instagram.com/ultralandsholdet_/" target="_blank" rel="noreferrer">
                   <img
                     src={instagram}
                     alt="Instagram"
+                    style={{ width: "1em", height: "1em" }}
+                  />
+                </a>
+                <a title="LinkedIn" href="https://www.linkedin.com/company/ultralandsholdet" target="_blank" rel="noreferrer">
+                  <img
+                    src={linkedin}
+                    alt="LinkedIn"
                     style={{ width: "1em", height: "1em" }}
                   />
                 </a>
